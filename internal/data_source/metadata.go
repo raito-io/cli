a40db0cc7f48dcd5b24e4528c032eabea6af639f
Dieter Wachters <dieter@raito.io>
2022-07-06 10:05:45 +0200

    Fixing serialization of MetaData struct

--- internal/data_source/metadata.go
+++ internal/data_source/metadata.go
@@ -3,6 +3,7 @@ package data_source
 import (
 	"encoding/json"
 	"fmt"
+	"regexp"
 	"strings"
 	"time"
 
@@ -15,16 +16,20 @@ func SetMetaData(config target.BaseTargetConfig, metadata data_source.MetaData) 
 	logger := config.Logger.With("datasource", config.DataSourceId)
 	start := time.Now()
 
-	mdb, err := json.Marshal(metadata)
+	mdm, err := marshalMetaData(metadata)
 	if err != nil {
 		return fmt.Errorf("error while serializing metadata information: %s", err.Error())
 	}
-
-	md := string(mdb)
+	md, err := fixMetaData(mdm)
+	if err != nil {
+		return err
+	}
+	// Additional escaping the quotes to embed in graphql string
+	md = strings.Replace(md, "\"", "\\\"", -1)
 
 	gqlQuery := fmt.Sprintf(`{ "operationName": "SetMetaData", "variables":{}, "query": "mutation SetMetaData {
         setDataSourceMetaData(id: \"%s\", input: %s) {
-          errors
+          id
         }
     }" }"`, config.DataSourceId, md)
 
@@ -38,4 +43,45 @@ func SetMetaData(config target.BaseTargetConfig, metadata data_source.MetaData) 
 	logger.Info(fmt.Sprintf("Done setting DataSource metadata in %s", time.Since(start).Round(time.Millisecond)))
 
 	return nil
+}
+
+func marshalMetaData(md data_source.MetaData) (string, error) {
+	mdb, err := json.Marshal(md)
+	if err != nil {
+		return "", fmt.Errorf("error while serializing metadata information: %s", err.Error())
+	}
+
+	return string(mdb), nil
+}
+
+func fixMetaData(input string) (string, error) {
+	md := input
+	reg, err := regexp.Compile("\"([a-zA-Z]+)\":")
+	if err != nil {
+		return "", fmt.Errorf("unable to compile regular expression: %s", err.Error())
+	}
+	md = reg.ReplaceAllString(md, "$1:")
+
+	// Remove all ',permissions:null'
+	reg2, err := regexp.Compile("(,)?permissions:null")
+	if err != nil {
+		return "", fmt.Errorf("unable to compile regular expression: %s", err.Error())
+	}
+	md = reg2.ReplaceAllString(md, "${1}permissions:[]")
+
+	// Remove all ',children:null'
+	reg3, err := regexp.Compile("(,)?children:null")
+	if err != nil {
+		return "", fmt.Errorf("unable to compile regular expression: %s", err.Error())
+	}
+	md = reg3.ReplaceAllString(md, "${1}children:[]")
+
+	// Remove all ',globalPermissions:null'
+	reg4, err := regexp.Compile("(,)?globalPermissions:null")
+	if err != nil {
+		return "", fmt.Errorf("unable to compile regular expression: %s", err.Error())
+	}
+	md = reg4.ReplaceAllString(md, "")
+
+	return md, nil
 }

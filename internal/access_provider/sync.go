24916688dcf795dce80c18175902c3052b5a4474
Dieter Wachters <dieter.wachters@gmail.com>
2022-08-31 10:15:57 +0200

    Parsing lastCalculated + added tests (#60)

--- internal/access_provider/sync.go
+++ internal/access_provider/sync.go
@@ -1,14 +1,16 @@
 package access_provider
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
+	"github.com/hashicorp/go-hclog"
 	dapc "github.com/raito-io/cli/base/access_provider"
 	baseconfig "github.com/raito-io/cli/base/util/config"
-
 	"github.com/raito-io/cli/internal/data_access"
 	"github.com/raito-io/cli/internal/file"
 	"github.com/raito-io/cli/internal/job"
@@ -22,7 +24,7 @@ type AccessProviderImportResult struct {
 	AccessRemoved int `json:"accessRemoved"`
 }
 
-var accessRightsLastUpdated int64 = 0
+var accessLastCalculated = map[string]int64{}
 
 type DataAccessSync struct {
 	TargetConfig  *target.BaseTargetConfig
@@ -48,15 +50,19 @@ func (s *DataAccessSync) StartSyncAndQueueJob(client plugin.PluginClient) (job.J
 		BaseTargetConfig: *s.TargetConfig,
 	}
 
+	lastUpdated := accessLastCalculated[s.TargetConfig.DataSourceId]
+
 	s.TargetConfig.Logger.Info("Fetching access providers for this data source from Raito")
-	dar, err := data_access.RetrieveDataAccessListForDataSource(&config, accessRightsLastUpdated)
+	dar, err := data_access.RetrieveDataAccessListForDataSource(&config, lastUpdated)
 
 	if err != nil {
 		return job.Failed, err
 	}
 
-	// TODO read this from the file
-	//accessRightsLastUpdated = dar.LastCalculated
+	err = s.updateLastCalculated(dar)
+	if err != nil {
+		return job.Failed, err
+	}
 
 	syncerConfig := dapc.AccessSyncConfig{
 		ConfigMap:  baseconfig.ConfigMap{Parameters: s.TargetConfig.Parameters},
@@ -94,6 +100,51 @@ func (s *DataAccessSync) StartSyncAndQueueJob(client plugin.PluginClient) (job.J
 	s.TargetConfig.Logger.Debug(fmt.Sprintf("Current status: %s", status.String()))
 
 	return status, nil
+}
+
+func (s *DataAccessSync) updateLastCalculated(filePath string) error {
+	time, err := findLastCalculated(filePath, s.TargetConfig.Logger)
+	if err != nil {
+		return err
+	}
+	accessLastCalculated[s.TargetConfig.DataSourceId] = time
+
+	return nil
+}
+
+func findLastCalculated(filePath string, logger hclog.Logger) (int64, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return 0, fmt.Errorf("error while reading temporary file %q: %s", filePath, err.Error())
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lineNr := 0
+	searchString := "lastCalculated:"
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if index := strings.Index(line, searchString); index >= 0 {
+			timeString := strings.TrimSpace(line[index+len(searchString):])
+			time, err := strconv.Atoi(timeString)
+
+			if err != nil {
+				return 0, fmt.Errorf("unable to parse lastCalculated field in %q: %s", filePath, err.Error())
+			}
+
+			return int64(time), nil
+		}
+
+		lineNr++
+
+		if lineNr == 10 {
+			logger.Info(fmt.Sprintf("Didn't find 'lastCalculated' field in first 10 lines of %q. Giving up.", filePath))
+			return 0, nil
+		}
+	}
+
+	return 0, nil
 }
 
 func (s *DataAccessSync) ProcessResults(results interface{}) error {

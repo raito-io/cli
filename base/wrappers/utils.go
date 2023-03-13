bfcbb75b592e5d6fe6e142865919df32e2060b11
Ruben Mennes <ruben.mennes@me.com>
2023-03-13 17:17:26 +0100

    No need to wrap time error anymore

--- base/wrappers/utils.go
+++ base/wrappers/utils.go
@@ -3,9 +3,6 @@ package wrappers
 import (
 	"time"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
 	"github.com/raito-io/cli/base"
 )
 
@@ -15,12 +12,6 @@ func timedExecution(f func() error) (time.Duration, error) {
 	start := time.Now()
 	err := f()
 	sec := time.Since(start).Round(time.Millisecond)
-
-	if err != nil {
-		if _, found := status.FromError(err); !found {
-			err = status.Error(codes.Internal, err.Error())
-		}
-	}
 
 	return sec, err
 }

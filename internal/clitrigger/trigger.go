d97afc0d206debd7e8ec90e2dfd6e5724fa40768
Ruben Mennes <ruben.mennes@me.com>
2023-03-28 12:52:04 +0200

    Fix disable websocket

--- internal/clitrigger/trigger.go
+++ internal/clitrigger/trigger.go
@@ -9,3 +9,21 @@ type CliTrigger interface {
 	Reset()
 	Wait()
 }
+
+var _ CliTrigger = (*DummyCliTrigger)(nil)
+
+type DummyCliTrigger struct {
+}
+
+func (d DummyCliTrigger) TriggerChannel(ctx context.Context) <-chan TriggerEvent {
+	ch := make(chan TriggerEvent)
+	defer close(ch)
+
+	return ch
+}
+
+func (d DummyCliTrigger) Reset() {
+}
+
+func (d DummyCliTrigger) Wait() {
+}

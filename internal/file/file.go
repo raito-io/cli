26c8bfa759112c6e666dc6026320b7b5babf2480
Ruben Mennes <ruben.mennes@me.com>
2025-02-25 16:09:40 +0100

    Checksums for files

--- internal/file/file.go
+++ internal/file/file.go
@@ -1,10 +1,13 @@
 package file
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 
@@ -12,29 +15,60 @@ import (
 	"github.com/raito-io/cli/internal/util/connect"
 )
 
+func hashFile(file string) (string, int64, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return "", 0, fmt.Errorf("unable to open file %q: %w", file, err)
+	}
+
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		return "", 0, fmt.Errorf("unable to get file info of %q: %w", file, err)
+	}
+
+	fileSize := fi.Size()
+
+	h := sha256.New()
+	if _, err = io.Copy(h, f); err != nil {
+		return "", 0, fmt.Errorf("unable to hash file %q: %w", file, err)
+	}
+
+	return base64.StdEncoding.EncodeToString(h.Sum(nil)), fileSize, nil
+}
+
 // UploadFile uploads the file from the given path.
 // It returns the key to use to pass to the Raito backend to use the file.
 func UploadFile(file string, config *types.BaseTargetConfig) (string, error) {
-	url, key, err := getUploadURL(config)
+	hash, fileSize, err := hashFile(file)
+	if err != nil {
+		return "", fmt.Errorf("hash file: %w", err)
+	}
+
+	url, key, headers, err := getUploadURL(config, hash, fileSize)
+
+	config.TargetLogger.Info(fmt.Sprintf("Uploading file %q to %q", file, url))
+
 	if err != nil {
 		return "", err
 	}
 
-	return uploadFileToBucket(file, config, url, key)
+	return uploadFileToBucket(file, config, url, key, headers)
 }
 
 // UploadLogFile uploads the file from the given path.
 // It returns the key to use to pass to the Raito backend to use the file.
 func UploadLogFile(file string, config *types.BaseTargetConfig, task string) (string, error) {
-	url, key, err := getUploadLogsURL(config, task)
+	url, key, _, err := getUploadLogsURL(config, task)
 	if err != nil {
 		return "", err
 	}
 
-	return uploadFileToBucket(file, config, url, key)
+	return uploadFileToBucket(file, config, url, key, map[string][]string{})
 }
 
-func uploadFileToBucket(file string, config *types.BaseTargetConfig, url string, key string) (string, error) {
+func uploadFileToBucket(file string, config *types.BaseTargetConfig, url string, key string, headers map[string][]string) (string, error) {
 	start := time.Now()
 
 	data, err := os.Open(file)
@@ -54,6 +88,12 @@ func uploadFileToBucket(file string, config *types.BaseTargetConfig, url string,
 		return "", fmt.Errorf("error while executing upload: %s", err.Error())
 	}
 	req.ContentLength = stat.Size()
+
+	for headerKey, headerValue := range headers {
+		for _, value := range headerValue {
+			req.Header.Add(headerKey, value)
+		}
+	}
 
 	client := &http.Client{}
 	res, err := client.Do(req)
@@ -80,40 +120,45 @@ func uploadFileToBucket(file string, config *types.BaseTargetConfig, url string,
 // GetUploadURL creates an S3 URL to upload a file to.
 // It returns the upload URL and the file key to use to pass to the Raito backend to use it.
 // Returns two empty strings if something went wrong (error logged)
-func getUploadURL(config *types.BaseTargetConfig) (string, string, error) {
-	return getUploadUrlAndKey(config, "file/upload/signed-url")
+func getUploadURL(config *types.BaseTargetConfig, hash string, fileSize int64) (string, string, map[string][]string, error) {
+	params := url.Values{}
+	params.Add("sha256", hash)
+	params.Add("contentLength", fmt.Sprintf("%d", fileSize))
+
+	return getUploadUrlAndKey(config, "file/upload/signed-url?"+params.Encode())
 }
 
-func getUploadLogsURL(config *types.BaseTargetConfig, task string) (string, string, error) {
+func getUploadLogsURL(config *types.BaseTargetConfig, task string) (string, string, map[string][]string, error) {
 	return getUploadUrlAndKey(config, "file/upload/logs/signed-url?task="+task)
 }
 
-func getUploadUrlAndKey(config *types.BaseTargetConfig, path string) (string, string, error) {
+func getUploadUrlAndKey(config *types.BaseTargetConfig, path string) (string, string, map[string][]string, error) {
 	resp, err := connect.DoGetToRaito(path, &config.BaseConfig)
 	if err != nil {
-		return "", "", fmt.Errorf("error while trying to get a signed upload URL: %s", err.Error())
+		return "", "", nil, fmt.Errorf("error while trying to get a signed upload URL: %s", err.Error())
 	}
 
 	if resp.StatusCode >= 300 {
-		return "", "", fmt.Errorf("error (HTTP %d) while trying to get a signed upload URL: %s", resp.StatusCode, resp.Status)
+		return "", "", nil, fmt.Errorf("error (HTTP %d) while trying to get a signed upload URL: %s", resp.StatusCode, resp.Status)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", "", fmt.Errorf("error while reading result body for getting signed url: %s", err.Error())
+		return "", "", nil, fmt.Errorf("error while reading result body for getting signed url: %s", err.Error())
 	}
 	var result signedURL
 
 	err = json.Unmarshal(body, &result)
 	if err != nil {
-		return "", "", fmt.Errorf("error while parsing result body for getting signed url: %s", err.Error())
+		return "", "", nil, fmt.Errorf("error while parsing result body for getting signed url: %s", err.Error())
 	}
 
-	return result.URL, result.Key, nil
+	return result.URL, result.Key, result.SignedHeaders, nil
 }
 
 type signedURL struct {
-	URL string
-	Key string
+	URL           string
+	Key           string
+	SignedHeaders map[string][]string `json:"signedHeaders,omitempty"`
 }
